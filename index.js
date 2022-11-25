fbcb2a26453ae4f51bf6eb0d3ea75cb225a9ba35
muntasir <sajeebmuntasir0@gmail.com>
2022-11-25 18:37:22 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
 require("dotenv").config()
 
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const uri = process.env.URI;
-console.log(uri)
+const client = new MongoClient(uri);
+
+const dbConnect = async () => {
+    try {
+        await client.connect();
+        console.log("db connected")
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+dbConnect();
+// console.log(uri)
+app.get("/", (req, res) => {
+    res.send("server is running")
+})
+app.listen(port, () => {
+    console.log("server is on")
+})
